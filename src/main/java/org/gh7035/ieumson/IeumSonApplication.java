a7fd7ed03b20c736bcb217579d4eee6bc2dcde44
gh7035 <gh7035@daum.net>
2026-05-27 10:31:13 +0900

    chore : project setting

---
+++ src/main/java/org/gh7035/ieumson/IeumSonApplication.java
@@ -0,0 +1,13 @@
+package org.gh7035.ieumson;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IeumSonApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IeumSonApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/gh7035/ieumson/IeumSonApplicationTests.java
@@ -0,0 +1,13 @@
+package org.gh7035.ieumson;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IeumSonApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
