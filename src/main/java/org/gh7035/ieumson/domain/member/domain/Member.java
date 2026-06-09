f6c3e917ed6294dff8fda52e439461d5a05bc520
gh7035 <gh7035@daum.net>
2026-06-09 12:12:24 +0900

    chore: project init

---
+++ src/main/java/org/gh7035/ieumson/domain/member/domain/Member.java
@@ -0,0 +1,12 @@
+package org.gh7035.ieumson.domain.member.domain;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+
+@Entity
+public class Member {
+    @Id
+    @GeneratedValue()
+
+}
---
+++ src/main/java/org/gh7035/ieumson/global/entity/BaseEntity.java
@@ -0,0 +1,4 @@
+package org.gh7035.ieumson.global.entity;
+
+public class BaseEntity {
+}
